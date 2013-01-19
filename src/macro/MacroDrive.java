c5b00e5e4b1fbc7644633f5f03f6ec06f5e30855
Calvin <smartycalvin@gmail.com>
2013-01-18 16:07:53 -0800

    Work on MacroDrive

--- src/macro/MacroDrive.java
+++ src/macro/MacroDrive.java
@@ -5,20 +5,68 @@
 package macro;
 
 import core.GRTMacro;
-import event.events.EncoderEvent;
-import event.listeners.EncoderListener;
-import logger.GRTLogger;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import mechanism.GRTDriveTrain;
+import sensor.GRTEncoder;
 
 /**
  *
  * @author keshav
  */
-public class MacroDrive extends GRTMacro implements EncoderListener {
+public class MacroDrive extends GRTMacro {
 
     private GRTDriveTrain dt;
     private double distance = 0;
     private double velocity = 1;
+    
+    private double leftInitialDistance;
+    private double rightInitialDistance;
+    
+    private PIDController leftDTController;
+    private PIDController rightDTController;
+    private GRTEncoder leftEncoder;
+    private GRTEncoder rightEncoder;
+    
+    private double leftSpeed;
+    private double rightSpeed;
+    
+    private static final double LP = 0.4;
+    private static final double LI = 0.0;
+    private static final double LD = 0.0;
+    private static final double RP = 0.4;
+    private static final double RI = 0.0;
+    private static final double RD = 0.0;
+    
+    private static final int POLL_TIME = 12;
+    private static final double TOLERANCE = 4; //TODO
+    
+    private PIDSource leftSource = new PIDSource() {
+        public double pidGet() {
+            return distance - (leftEncoder.getDistance() - leftInitialDistance);
+        }
+    };
+    
+    private PIDOutput leftOutput = new PIDOutput() {
+        public void pidWrite(double output) {
+            leftSpeed = output;
+            dt.setMotorSpeeds(leftSpeed, rightSpeed);
+        }
+    };
+    
+    private PIDSource rightSource = new PIDSource() {
+        public double pidGet() {
+            return distance - (rightEncoder.getDistance() - rightInitialDistance);
+        }
+    };
+    
+    private PIDOutput rightOutput = new PIDOutput() {
+        public void pidWrite(double output) {
+            rightSpeed = output;
+            dt.setMotorSpeeds(leftSpeed, rightSpeed);
+        }
+    };
 
     /*
      * Creates a new Driving Macro
@@ -27,51 +75,43 @@ public class MacroDrive extends GRTMacro implements EncoderListener {
      * @param distance distance to travel in m (assumes travel in straight line)
      * @param timeout time in ms
      */
-    public MacroDrive(GRTDriveTrain dt, double distance, int timeout) {
+    public MacroDrive(GRTDriveTrain dt, double distance, int timeout,
+            GRTEncoder leftEncoder, GRTEncoder rightEncoder) {
         super("Drive Macro", timeout);
         this.dt = dt;
         this.distance = distance;
+        this.leftEncoder = leftEncoder;
+        this.rightEncoder = rightEncoder;
     }
 
-    protected void initialize() {
-        dt.getLeftEncoder().addListener(this);
-        dt.getRightEncoder().addListener(this);        
+    protected void initialize() {        
         dt.setMotorSpeeds(velocity, velocity);
+        
+        leftInitialDistance = leftEncoder.getDistance();
+        rightInitialDistance = rightEncoder.getDistance();
+        
+        leftDTController = new PIDController(LP, LI, LD, leftSource, leftOutput, POLL_TIME);
+        rightDTController = new PIDController(RP, RI, RD, rightSource, rightOutput, POLL_TIME);
+        
+        leftDTController.setAbsoluteTolerance(TOLERANCE);
+        rightDTController.setAbsoluteTolerance(TOLERANCE);
+        
+        leftDTController.setSetpoint(distance);
+        rightDTController.setSetpoint(distance);
+        
+        leftDTController.setOutputRange(-1.0, 1.0);
+        rightDTController.setOutputRange(-1.0, 1.0);
+        
     }
 
     protected void perform() {
-        //TODO
+        if (leftDTController.onTarget() && rightDTController.onTarget())
+            hasCompletedExecution = true;
     }
 
     public void die() {
-        dt.getLeftEncoder().removeListener(this);
-        dt.getRightEncoder().removeListener(this);
         dt.setMotorSpeeds(0, 0);
-    }
-
-    public void rotationStarted(EncoderEvent e) {
-    }
-
-    public void degreeChanged(EncoderEvent e) {
-    }
-
-    public void distanceChanged(EncoderEvent e) {
-//        if(e.getSource() == dt.getLeftEncoder()){
-//            GRTLogger.logInfo("Dist: " + e.getData());
-//            if (e.getData() > distance) {
-//                velocity = 0;
-//                dt.setMotorSpeeds(0, 0);
-//                die();
-//            } else {
-//                dt.setMotorSpeeds(velocity, velocity);
-//            }
-//        }        
-    }
-
-    public void rotationStopped(EncoderEvent e) {
-    }
-
-    public void rateChanged(EncoderEvent e) {
-        GRTLogger.logInfo("[" + e.getSource() + "]: " + e.getData());
+        leftDTController.free();
+        rightDTController.free();
     }
 }
